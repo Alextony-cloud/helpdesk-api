eb5eb25e9cfdbe427da45d1e5ad8c8998a7a41b3
Alextony-cloud <alextonyrodrigues@gmail.com>
2024-04-18 20:03:05 -0300

    findAll implementado para chamado

--- src/main/java/io/github/alextonycloud/helpdesk/resources/ChamadoResource.java
+++ src/main/java/io/github/alextonycloud/helpdesk/resources/ChamadoResource.java
@@ -1,6 +1,10 @@
 package io.github.alextonycloud.helpdesk.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +23,17 @@ public class ChamadoResource {
 		this.service = service;
 	}
 
-	@RequestMapping("/{id}")
+	@GetMapping("/{id}")
 	public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id){
 		Chamado obj = service.findById(id);
 		return ResponseEntity.ok().body(new ChamadoDTO(obj));
 	}
+	
+	@GetMapping
+	public ResponseEntity<List<ChamadoDTO>> findAll(){
+		List<Chamado> list = service.findAll();
+		List<ChamadoDTO> listDTO = list.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+	
 }
--- src/main/java/io/github/alextonycloud/helpdesk/service/ChamadoService.java
+++ src/main/java/io/github/alextonycloud/helpdesk/service/ChamadoService.java
@@ -1,5 +1,6 @@
 package io.github.alextonycloud.helpdesk.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
@@ -20,6 +21,10 @@ public class ChamadoService {
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id));
+	}
+
+	public List<Chamado> findAll() {
+		return repository.findAll();
 	}
 	
 }
